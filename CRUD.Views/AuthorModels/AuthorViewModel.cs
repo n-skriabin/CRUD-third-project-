c31c077c57c8c4dd931c35ef66572789a643a030
Anuitex-18 <deltagroup@anuitex.com>
2018-01-10 17:28:50 +0200

    Refactoring.

--- CRUD.Views/AuthorModels/AuthorViewModel.cs
+++ CRUD.Views/AuthorModels/AuthorViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CRUD.Views
+﻿namespace CRUD.Views
 {
     public class AuthorViewModel : BaseEntityViewModel
     {       
--- CRUD.Views/BookModels/BookViewModel.cs
+++ CRUD.Views/BookModels/BookViewModel.cs
@@ -1,9 +1,5 @@
 ﻿using CRUD.DataAccess;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CRUD.Views
 {
